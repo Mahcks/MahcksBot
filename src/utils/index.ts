2207209fc185ba0845c169103841140a53a0dd0a
Max <maxnatural13@gmail.com>
2022-03-06 16:44:26 -0600

    Added bot ignore list

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -177,6 +177,8 @@ export async function getAllChatters(channel: string) {
 }
 
 export async function logMessage(channel: string, id: number, username: string, message: string, timestamp: Date) {
+  let ignoredBots: string[] = ['mahcksbot', 'streamelements', 'Supibot'];
+  if (ignoredBots.includes(username)) return;
   await insertRow('INSERT INTO logs (channel, id, username, message, timestamp) VALUES (?, ?, ?, ?, ?);', [channel, id, username, message, timestamp]);
 }
 
