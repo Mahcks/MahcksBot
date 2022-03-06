ee3796619101bf6d9905941bc6c50e6710c3c284
Max <maxnatural13@gmail.com>
2022-03-06 02:18:16 -0600

    added pyramid

--- src/commands/pattern/index.ts
+++ src/commands/pattern/index.ts
@@ -27,19 +27,39 @@ const patternCommand: CommandInt = {
     let input = context[1];
 
     let normal = '⠀⠀⠀⠀ ⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀  (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote) ⠀⠀⠀⠀ ⠀ ⠀ (emote)';
-    let space  = '★⠀ ｡･⠀ (emote) ⠀⠀ﾟ⠀⠀｡⠀ (emote) ☆ ⠀ ･･⠀⠀ (emote) ★⠀ ⠀ *⠀ﾟ⠀⠀☆⠀⠀ ｡･ (emote) ⠀⠀★ ⠀ ⠀ *⠀ﾟ⠀ ⠀ ★⠀ ｡･⠀ (emote) ∴⠀ ﾟ * ⠀ﾟ⠀ (emote) ⠀⠀ﾟ⠀⠀｡⠀⠀☆⠀⠀⠀ ｡･ (emote) ★ ⠀*⠀ﾟ⠀⠀ ｡･ (emote) ⠀★ ';
+    let space = '★⠀ ｡･⠀ (emote) ⠀⠀ﾟ⠀⠀｡⠀ (emote) ☆ ⠀ ･･⠀⠀ (emote) ★⠀ ⠀ *⠀ﾟ⠀⠀☆⠀⠀ ｡･ (emote) ⠀⠀★ ⠀ ⠀ *⠀ﾟ⠀ ⠀ ★⠀ ｡･⠀ (emote) ∴⠀ ﾟ * ⠀ﾟ⠀ (emote) ⠀⠀ﾟ⠀⠀｡⠀⠀☆⠀⠀⠀ ｡･ (emote) ★ ⠀*⠀ﾟ⠀⠀ ｡･ (emote) ⠀★ ';
     let what = '❓ ⠀ ⠀ ⠀ ❓ ⠀ ⠀ ⠀ ⠀ ⠀ ❓ ⠀ ⠀ ⠀ ⠀ ⠀⠀⠀ ❓ ⠀ ⠀⠀ ⠀ ❓ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀⠀ ❓ ⠀ ⠀ ⠀ ❓ ⠀ ⠀⠀ ❓ ⠀ ⠀⠀ ⠀ (emote) ⠀⠀ ⠀ ❓ ⠀ ⠀ ⠀⠀ ⠀ ❓ ⠀ ⠀ ⠀⠀⠀ ⠀ ❓ ⠀ ⠀ ⠀ ⠀ ❓';
 
     let chosen;
     if (!input) chosen = normal;
 
     if (input === "space") {
-      chosen = space;
+      return sendMessage(client, false, channel, space.replace(/(\(emote\))/gm, context[0]));
     } else if (input === "what") {
-      chosen = what;
-    } else chosen = normal;
+      return sendMessage(client, false, channel, what.replace(/(\(emote\))/gm, context[0]));
+    } else if (input === "pyramid") {
+      const createPyramid = (height: number) => {
+        for (var i = 1; i <= height; i++) {
+          var row = '';
+  
+          for (var j = 1; j <= i; j++)
+            row += " " + context[0];
+          sendMessage(client, false, channel, row);
+        }
+        for (var i = height - 1; i > 0; i--) {
+          var row = '';
+  
+          for (var j = i; j > 0; j--)
+            row += " " + context[0];
+          sendMessage(client, false, channel, row);
+        }
+      }
 
-    return sendMessage(client, false, channel, chosen?.replace(/(\(emote\))/gm, context[0]));
+      createPyramid(5);
+
+    } else chosen = sendMessage(client, false, channel, normal.replace(/(\(emote\))/gm, context[0]));;
+
+    
   }
 }
 
