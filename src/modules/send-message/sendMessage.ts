073eed36cfdb5d435f7c45fdc4df1908b35d57c5
Max <maxnatural13@gmail.com>
2022-03-06 22:30:03 -0600

    fixed sendMessage

--- src/modules/send-message/sendMessage.ts
+++ src/modules/send-message/sendMessage.ts
@@ -3,8 +3,6 @@ import config from "../../config/config";
 
 let alternate = false;
 export default async (client: Actions, channel: string, message: string) => {
-  (message.startsWith("#")) ? channel = channel.substring(1) : channel;
-  if (config.production && channel === "mahcksimus") return;
   let msg = (alternate) ? message.concat(" 󠀀") : message;
   (alternate) ? alternate = false : alternate = true;
 
