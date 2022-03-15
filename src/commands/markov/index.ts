27513d94125140fff486ede42e2be6c74929e20a
Max <maxnatural13@gmail.com>
2022-03-14 22:58:44 -0500

    Changed max length to 150

--- src/commands/markov/index.ts
+++ src/commands/markov/index.ts
@@ -43,7 +43,7 @@ const markovCommand: CommandInt = {
       maxTries: 10000,
 
       filter: (result: any) => {
-        return result.string.split(' ').length <= 30 && !result.string.includes("⣿") && result.score >= 50
+        return result.string.split(' ').length <= 150 && !result.string.includes("⣿") && result.score >= 50
       }
     }
 
